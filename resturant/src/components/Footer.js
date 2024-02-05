dfb4e5ecc6bfbff6592457512c6119850569ab91
ChetnaMaske <maskechetna2000@gmail.com>
2024-02-05 23:32:04 +0530

    add Footer

--- resturant/src/components/Footer.js
+++ resturant/src/components/Footer.js
@@ -4,9 +4,13 @@ import '../styles/Footer.css'
 function Footer() {
   return (
     <div className='footer'>
-        <div className=''>
-            chetna maske 2024 pizzahub.com
+        <div className='socialMedia'>
+          <i class="fa-brands fa-instagram"></i>
+          <i class="fa-brands fa-twitter"></i>
+          <i class="fa-brands fa-facebook"></i> 
+          <i class="fa-brands fa-youtube"></i>
         </div>
+        <p>ChetnaMaske 2024 pizzahub.com</p>
     </div>
   )
 }
